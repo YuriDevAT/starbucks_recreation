6882ac467257e2a6504a987fcf04365db7a5221a
Julia Undeutsch <54622834+saliainred@users.noreply.github.com>
2020-11-23 18:40:12 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-
+function myFunction() {
+  var x = document.getElementById("links");
+  if(x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
